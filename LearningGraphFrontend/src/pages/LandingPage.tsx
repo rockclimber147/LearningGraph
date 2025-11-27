23ac9fd39c1578004c7b51088271b0680f0c905b
Daylen Smith <daylencsmith@gmail.com>
2025-11-27 00:27:39 -0800

    oof styling

--- LearningGraphFrontend/src/pages/LandingPage.tsx
+++ LearningGraphFrontend/src/pages/LandingPage.tsx
@@ -5,12 +5,19 @@ export default function LandingPage() {
   const { username, logout } = useAuth();
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Welcome, {username}!</h1>
-      <button onClick={logout}>Logout</button>
-
-      {/* Render the reusable canvas */}
-      <GraphCanvas width={800} height={600} />
+    <div>
+      <div style={{ padding: 20 }}>
+        <div className="centered-button">
+          <h1>Welcome, {username}!</h1>
+        </div>
+        <div className="centered-button">
+          {/* Render the reusable canvas */}
+          <GraphCanvas width={800} height={600} />
+        </div>
+      </div>
+      <div className="centered-button">
+        <button onClick={logout}>Logout</button>
+      </div>
     </div>
   );
 }
